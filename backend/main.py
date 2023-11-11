3bf875aae729b1ab576528289884dbfaeff0b3ad
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2023-11-11 04:18:07 -0500

    Fix access to frontend
    Requests made to backend are still not working.
    I get an ECONNREFUSED error.

--- backend/main.py
+++ backend/main.py
@@ -170,4 +170,5 @@ app.include_router(router)
 
 
 if __name__ == "__main__":
-    uvicorn.run('main:app', reload=True)
+    uvicorn.run('main:app', host="127.0.0.1", port=8000,
+                reload=True, log_level="info")
